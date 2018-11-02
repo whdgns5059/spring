66db85868d2f68431a0d2ca25cc71c9d817ee022
WaterSpary_DDIT <whdgns5059@gmail.com>
2018-11-02 18:01:50 +0900

    ModelAttribute

---
+++ src/test/java/kr/or/ddit/mvc/MvcControllerTest.java
@@ -0,0 +1,54 @@
+package kr.or.ddit.mvc;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath:kr/or/ddit/config/spring/servlet-context.xml"})
+@WebAppConfiguration
+public class MvcControllerTest {
+
+	@Autowired
+	private WebApplicationContext ctx;
+	
+	private MockMvc mockMvc;
+
+	@Before
+	public void setUp() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+	}
+	@Test
+	public void test() throws Exception {
+		/***Given***/
+		
+		/***When***/
+		MvcResult mvcResult = mockMvc.perform(get("/mvc/view")).andReturn();
+		ModelAndView mav = mvcResult.getModelAndView();
+
+		
+		List<String> rangers = (List<String>) mav.getModel().get("rangers");
+		
+		/***Then***/
+		assertEquals("mvc/view", mav.getViewName());
+		assertEquals(4, rangers.size());
+		
+		
+	}
+
+}
